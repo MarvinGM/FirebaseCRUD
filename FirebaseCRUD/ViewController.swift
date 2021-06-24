8d6565c5b267b130d2746c5714997b11b2ef5070
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-24 16:34:39 +0800

    Version 1.0

--- FirebaseCRUD/ViewController.swift
+++ FirebaseCRUD/ViewController.swift
@@ -4,14 +4,48 @@
 //
 //  Created by Amben on 6/24/21.
 //
-
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let database = Database.database().reference()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        database.child("Something").observeSingleEvent(of: .value, with: { snapshot in
+            guard let value = snapshot.value as? [String: Any] else {
+                return
+            }
+            print("Value: \(value)")
+        })
+        
+        
+        
+        let button = UIButton(frame: CGRect(x: 20,
+                                            y: 200,
+                                            width: view.frame.size.width-40,
+                                            height: 50))
+        
+        button.setTitle("Add Entry", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .link
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
+        
+        
+    }
+    
+    @objc private func addNewEntry() {
+        let object: [String: Any] = [
+            "name": "Marvin" as NSObject,
+            "youtube": "Yes"
+        ]
+        database.child("Something_\(Int.random(in: 0..<100))").setValue(object)
     }
 
 
